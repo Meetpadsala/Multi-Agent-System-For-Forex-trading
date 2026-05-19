1b7fb8edfb7b483e5e60052a6c1f27037a9b7f81
Meetpadsala <2402031030051@silveroakuni.ac.in>
2026-05-19 11:51:12 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -89,9 +89,13 @@ WSGI_APPLICATION = 'wsgi.application'
 # }
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
